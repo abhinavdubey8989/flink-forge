1f1b928a6d2049a8ecbefe0201e65654795efb6d
abhinavdubey8989 <abhinavdubey8989@gmail.com>
2026-05-27 15:55:55 +0530

    1st commit

---
+++ src/main/java/com/flink_forge/FlinkForgeApplication.java
@@ -0,0 +1,13 @@
+package com.flink_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flink_forge/FlinkForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flink_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
